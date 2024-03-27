e1bcb60023218db3f8fcd670ad468b9ec1ffda38
Sahil Yousaf <sahilyousaf66@gmail.com>
2024-03-28 02:55:35 +0500

    second

--- index.ts
+++ index.ts
@@ -14,59 +14,59 @@ let answerPin = await inquirer.prompt([
 ]);
 if (answerPin.pin === myPin) {
     console.log(`\t"Correct Pin"\n`);
-    let operationAns= await inquirer.prompt([
+    let operationAns = await inquirer.prompt([
         {
-            name:"operations",
-            message:"Please Select Option",
-            type:"list",
-            choices:["Withdraw","Check Balance","Fast Cash"]
+            name: "operations",
+            message: "Please Select Option",
+            type: "list",
+            choices: ["Withdraw", "Check Balance", "Fast Cash"]
         }
     ]);
-    if(operationAns.operations=="Withdraw"){
-        let amountAns=await inquirer.prompt([
+    if (operationAns.operations == "Withdraw") {
+        let amountAns = await inquirer.prompt([
             {
-                name:"amount",
-                message:"How much do you want to withdraw?",
-                type:"number"
-        
+                name: "amount",
+                message: "How much do you want to withdraw?",
+                type: "number"
+
             }
         ]);
-        if(amountAns.amount<=myBalance){
-            myBalance-=amountAns.amount;
+        if (amountAns.amount <= myBalance) {
+            myBalance -= amountAns.amount;
             console.log(`\tYour remaining Balance Is: $${myBalance}\n`);
-            
+
         }
-        else{
+        else {
             console.log(`\t"Insufficient Balance"\n`);
-            
+
         };
     }
-    else if(operationAns.operations=="Check Balance"){
+    else if (operationAns.operations == "Check Balance") {
         console.log(`\tYour Balance Is: $${myBalance}\n`);
-        
+
     }
-    else if(operationAns.operations=="Fast Cash"){
-        let fastCashAns=await inquirer.prompt([
+    else if (operationAns.operations == "Fast Cash") {
+        let fastCashAns = await inquirer.prompt([
             {
-                name:"fastCash",
-                message:"How much amount you want to fast cash: ",
-                type:"list",
-                choices:[
-                    1000,2000,4000,10000
+                name: "fastCash",
+                message: "How much amount you want to fast cash: ",
+                type: "list",
+                choices: [
+                    1000, 2000, 4000, 10000
                 ]
             }
         ])
-        myBalance-=fastCashAns.fastCash
+        myBalance -= fastCashAns.fastCash
         console.log("You have successfully completed your transaction.\n");
-        
+
         console.log(`\tYour remaining Balance is: ${myBalance}\n`);
-        
+
     }
-    
+
 }
-else{
+else {
     console.log(`"Incorrect Pin"\n`);
-    
+
 };
 
 
